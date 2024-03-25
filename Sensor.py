1179aa87e9d5190571c6636d5a426c9a77eedfc4
Staker9 <127483554+Staker9@users.noreply.github.com>
2024-03-25 14:54:42 +0900

    Update Sensor.py

--- Sensor.py
+++ Sensor.py
@@ -2,13 +2,14 @@
 
 def add_breeze(grid, x, y, grid_size):
     for i, j in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-        if 0 <= i < grid_size and 0 <= j < grid_size:
+        if 0 <= i < grid_size and 0 <= j < grid_size and 'Wall' not in grid[i][j]:
             grid[i][j].append('Br')
 
 def add_glitter(grid, x, y):
-    grid[x][y].append('G')
+    if 'Wall' not in grid[x][y]:
+        grid[x][y].append('G')
 
 def add_stench(grid, x, y, grid_size):
     for i, j in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-        if 0 <= i < grid_size and 0 <= j < grid_size:
+        if 0 <= i < grid_size and 0 <= j < grid_size and 'Wall' not in grid[i][j]:
             grid[i][j].append('S')
