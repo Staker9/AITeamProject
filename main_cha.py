d72e3ebecad8e08767e18def041cad1abfcf31ff
Staker9 <127483554+Staker9@users.noreply.github.com>
2024-03-27 18:18:32 +0900

    Add files via upload

---
+++ main_cha.py
@@ -0,0 +1,12 @@
+import Wumpus
+import Agent_cha
+
+# GridWorld 인스턴스 생성
+world = Wumpus.GridWorld()
+world.setup_grid()
+
+# Agent 인스턴스 생성
+agent = Agent.Agent(grid_size=world.grid_size)
+
+# 수정된 print_grid 호출 방식
+world.print_grid(agent.x, agent.y)
