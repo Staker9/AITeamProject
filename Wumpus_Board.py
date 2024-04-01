6c7a4eedb7290e0ba5b048997dcf2480dc996822
kimsi1854 <103935651+kimsi1854@users.noreply.github.com>
2024-04-01 15:49:03 +0900

    Update Wumpus_Board.py

--- Wumpus_Board.py
+++ Wumpus_Board.py
@@ -57,46 +57,8 @@ world.setup_grid()
 
 ##################################################################
 #0401시영 색깔추가
-import Agent
-from Sensor import add_breeze, add_glitter, add_stench
-import numpy as np
-
 from colorama import Fore, Style
 
-
-
-class GridWorld:
-
-    def __init__(self):
-        self.grid_size = 6
-        self.grid = [[[] for _ in range(self.grid_size)] for _ in range(self.grid_size)]
-
-    def setup_grid(self):
-        # 테두리를 Wall로 설정
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                if i == 0 or i == self.grid_size - 1 or j == 0 or j == self.grid_size - 1:
-                    self.grid[i][j].append('~')
-                    #wall '~'로 변경 0401시영
-
-        # 1,1은 Safe로, 4,4는 Gold로 설정
-        self.grid[1][1].append('Safe')
-        self.grid[4][4].append('Gold')
-        add_glitter(self.grid, 4 - 1, 4)
-        add_glitter(self.grid, 4, 4 - 1)
-
-        # 나머지 위치에 대한 설정
-        for i in range(1, self.grid_size - 1):
-            for j in range(1, self.grid_size - 1):
-                if (i, j) not in [(1, 1), (4, 4)]:
-                    if np.random.rand() < 0.1:
-                        self.grid[i][j].append('Pit')
-                        add_breeze(self.grid, i, j, self.grid_size)
-                    elif np.random.rand() < 0.1:
-                        self.grid[i][j].append('Wumpus')
-                        add_stench(self.grid, i, j, self.grid_size)
-
-    #색깔추가, Step 추가 0401시영
     def print_grid(self, agent_x=None, agent_y=None, step=None):
         if step is not None:
             print(f"Step: {step}")  # 단계 출력
