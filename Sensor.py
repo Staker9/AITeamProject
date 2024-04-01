355b746f64ceafd1706ffb2ed263b3ef53275f3f
kimsi1854 <103935651+kimsi1854@users.noreply.github.com>
2024-04-01 19:28:19 +0900

    Update Sensor.py

--- Sensor.py
+++ Sensor.py
@@ -1,15 +1,17 @@
 # Sensor.py 파일
+#0401 시영
+#6, 15 line 'Wall' -> '~'로 변경
 
 def add_breeze(grid, x, y, grid_size):
     for i, j in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-        if 0 <= i < grid_size and 0 <= j < grid_size and 'Wall' not in grid[i][j]:
+        if 0 <= i < grid_size and 0 <= j < grid_size and '~' not in grid[i][j]:
             grid[i][j].append('Br')
 
 def add_glitter(grid, x, y):
-    if 'Wall' not in grid[x][y]:
-        grid[x][y].append('G')
+    if '~' not in grid[x][y]:
+        grid[x][y].append('glit')
 
 def add_stench(grid, x, y, grid_size):
     for i, j in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-        if 0 <= i < grid_size and 0 <= j < grid_size and 'Wall' not in grid[i][j]:
+        if 0 <= i < grid_size and 0 <= j < grid_size and '~' not in grid[i][j]:
             grid[i][j].append('S')
