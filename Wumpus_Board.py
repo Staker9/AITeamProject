9365571776cf573ab2f4ead9c98e1ff19a32a5e3
kimsi1854 <103935651+kimsi1854@users.noreply.github.com>
2024-04-01 19:33:30 +0900

    Update Wumpus_Board.py

--- Wumpus_Board.py
+++ Wumpus_Board.py
@@ -1,6 +1,14 @@
+#0401 시영
+#Agent 파란색/ pit, Wumpus 빨간색/ Gold 노란색
+#움직일때마다 Step 추가
+#25 line 'Wall'-> '~' 로 수정
+
+
 import Agent
 from Sensor import add_breeze, add_glitter, add_stench
 import numpy as np
+from colorama import Fore, Style
+
 
 
 class GridWorld:
@@ -14,7 +22,8 @@ class GridWorld:
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 if i == 0 or i == self.grid_size - 1 or j == 0 or j == self.grid_size - 1:
-                    self.grid[i][j].append('Wall')
+                    self.grid[i][j].append('~')
+                    
 
         # 1,1은 Safe로, 4,4는 Gold로 설정
         self.grid[1][1].append('Safe')
@@ -33,32 +42,7 @@ class GridWorld:
                         self.grid[i][j].append('Wumpus')
                         add_stench(self.grid, i, j, self.grid_size)
 
-    def print_grid(self, agent_x=None, agent_y=None):
-        cell_width = 14
-        horizontal_line = "-" * (self.grid_size * (cell_width + 1) + 1)
-
-        for i, row in enumerate(self.grid):
-            print(horizontal_line)
-            for j, cell in enumerate(row):
-                if agent_x == i and agent_y == j:
-                    cell_content = 'Agent'.center(cell_width)
-                elif not cell:
-                    cell_content = 'None'.center(cell_width)
-                else:
-                    cell_content = ','.join(cell).center(cell_width)
-                print("|" + cell_content, end="")
-            print("|")
-        print(horizontal_line)
-
-
-world = GridWorld()
-world.setup_grid()
-#world.print_grid()
-
-##################################################################
-#0401시영 색깔추가
-from colorama import Fore, Style
-
+    #색깔추가, Step 추가 0401시영
     def print_grid(self, agent_x=None, agent_y=None, step=None):
         if step is not None:
             print(f"Step: {step}")  # 단계 출력
@@ -90,5 +74,5 @@ from colorama import Fore, Style
 
 world = GridWorld()
 world.setup_grid()
-#world.print_grid()
+world.print_grid()
 
