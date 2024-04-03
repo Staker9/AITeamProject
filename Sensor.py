fa81c98cf26c12dfec3c993a14ddfc5d6bfa882f
Staker9 <127483554+Staker9@users.noreply.github.com>
2024-04-03 14:31:52 +0900

    Update Sensor.py

--- Sensor.py
+++ Sensor.py
@@ -2,6 +2,9 @@
 #0401 시영
 #6, 15 line 'Wall' -> '~'로 변경
 
+#0403 재현
+#S 제거함수 추가
+
 def add_breeze(grid, x, y, grid_size):
     for i, j in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
         if 0 <= i < grid_size and 0 <= j < grid_size and '~' not in grid[i][j]:
@@ -15,3 +18,8 @@ def add_stench(grid, x, y, grid_size):
     for i, j in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
         if 0 <= i < grid_size and 0 <= j < grid_size and '~' not in grid[i][j]:
             grid[i][j].append('S')
+            
+def del_stench(grid, x, y, grid_size):
+    for i, j in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+        if 0 <= i < grid_size and 0 <= j < grid_size and '~' not in grid[i][j]:
+            grid[i][j].remove('S')
